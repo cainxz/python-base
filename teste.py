eb118db2c11bf994fbeba2f4f8bec5b1513137c6
Kaio Miranda <mirandakaio483@gmail.com>
2025-04-30 19:38:37 -0300

    adicionado shebang e um comentario

--- teste.py
+++ teste.py
@@ -2,4 +2,4 @@
 
 #em baixo temos um comando chamado print que escreve o que for colocado dentro da chave e aspas
 
-print ("meu amigo é gayaaaaasssssssssssssssssssssssssssssssssssssssssssssssssssssa")
+print ("meu amigo é gayaaaaassssssssssssasssssssssssssssssssssssssssssssssssssssssa")
