4268b8c44a4f9848b66351b52267434d511681a8
Kaio Miranda <mirandakaio483@gmail.com>
2025-04-30 19:31:58 -0300

    teste do save

--- teste.py
+++ teste.py
@@ -1,1 +1,5 @@
+#!/usr/bin/env python3
+
+#em baixo temos um comando chamado print que escreve o que for colocado dentro da chave e aspas
+
 print ("meu amigo é gayaaaaasssssssssssssssssssssssssssssssssssssssssssssssssssssa")
