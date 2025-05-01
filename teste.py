e49d06078c4cf75bdee2a087596477504634e1e1
Kaio Miranda <mirandakaio483@gmail.com>
2025-05-01 20:58:04 -0300

    teste novo

--- teste.py
+++ teste.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
+"""
+hello world multi linguas
+
+dependendo da lingua configurada no ambiente o programa exibe a mensagem correspondente.
+
+como usar: 
+
+tenha a variavel lang devidamente coonfigurada ex: 
+
+    export LANG=pt_BR
+
+Execução: 
+
+  python3 hello.py
+  ou
+  ./hello.py
+"""
+__version__ = "0.0.1"
+__author__= "kaio"
+__license__= "Unlicense"
+
 
 #em baixo temos um comando chamado print que escreve o que for colocado dentro da chave e aspas
 
