f997f32cacdd0431d9379ba98a84543c341e3341
Kaio Miranda <mirandakaio483@gmail.com>
2025-05-24 18:04:08 -0300

    minha primeira tentativa de codigo sem auxilio

--- tabuada.py
+++ tabuada.py
@@ -1,24 +1,45 @@
 #!usr/bin/env python
-#"""imprime a tabuada do 1 ao 10."""
+"""imprime a tabuada do 1 ao 10.
 
+-----tabuada do 1 -----
+1 x 1 = 1
+1 x 2 = 2
+1 x 3 - 3
 
-__version__ = "0.1.00"
+...
+##########################
+-----Tabuada do 2 -----
+2 x 1 = 2
+2 x 2 = 4
+3 x 3 = 6
+...
+###############################
+
+"""
+
+__version__ = "0.1.1"
 __author__ = "Kaio"
-
-#numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 numeros = list(range(1, 11))
 
-#print  (numeros)
+
+for n1 in numeros:
+    print("{:-^18}".format(f"Tabuada do {n1}"))
+    print()
+    for n2 in numeros:  
+        resultado = n1 / n2  
+        print("{:^18}".format(f"{n1} x {n2} = {resultado}"))
+                
+
+    print("#" * 18)        
+            
 
 
 
-#iterable (percorriveis)
 
-#para cada numeroo em numeros: 
 
-for numero in numeros:
-        print("tabuada do:", numeros)
-        for outro_numero in numeros: 
-            print(numero * outro_numero)
-        print ("-------------")
-        
+
+
+
+
+
+
